73d415d65aebebc888dc58ed4ffa7494a98068eb
Luke Frake <lukefrake@gmail.com>
2015-08-11 21:38:30 +0100

    Try something

--- Gruntfile.js
+++ Gruntfile.js
@@ -283,7 +283,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: '../',
+          remote: 'master',
           branch: 'master',
           commit: true,
           push: true
